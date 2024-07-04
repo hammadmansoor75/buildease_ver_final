55748c2d665ec5d7d2cb39a2e86818dec023cc85
Hammad Mansoor <hammadmansoor75@gmail.com>
2024-07-04 17:49:58 +0500

    resources page updated

--- app/(dashboard)/[projectId]/(routes)/resources/page.tsx
+++ app/(dashboard)/[projectId]/(routes)/resources/page.tsx
@@ -1,36 +1,42 @@
-import React from 'react'
-import prismadb from '@/lib/prismadb'
-import ResourcesClient from './components/client'
+import React from 'react';
+import prismadb from '@/lib/prismadb';
+import ResourcesClient from './components/client';
 
-export const Resources = async({
-  params
-} : {
-  params : {projectId : string}
-}) => {
-  const resources = await prismadb.resource.findMany({
-    where : {
-      projectId : params.projectId
-    },
-    orderBy : {
-      createdAt : "desc"
-    }
-  })
-
+const Resources = ({ resources }) => {
   const formattedResources = resources.map((item) => ({
-    id : item.id,
-    name : item.name,
-    status : item.status,
-    assignedToTeam : item.assignedToTeam,
-    assignedFrom : item.assignedFrom,
-    assignedTill : item.assignedTill
-  }))
+    id: item.id,
+    name: item.name,
+    status: item.status,
+    assignedToTeam: item.assignedToTeam,
+    assignedFrom: item.assignedFrom,
+    assignedTill: item.assignedTill
+  }));
+
   return (
     <div className='flex-col'>
-        <div className='flex-1 space-y-4 p-8 pt-6'>
-           <ResourcesClient data ={resources} />
-        </div>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        <ResourcesClient data={formattedResources} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Resources
+export const getServerSideProps = async (context) => {
+  const { projectId } = context.params;
+  const resources = await prismadb.resource.findMany({
+    where: {
+      projectId: projectId
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  });
+
+  return {
+    props: {
+      resources
+    }
+  };
+};
+
+export default Resources;
